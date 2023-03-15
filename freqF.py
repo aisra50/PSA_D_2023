71f8f142eb492d7be9a406c53c44fa9465de7e45
reisa <70296556+aisra50@users.noreply.github.com>
2023-03-15 16:09:23 -0300

    Update freqF.py

--- freqF.py
+++ freqF.py
@@ -6,7 +6,6 @@ while 1 :
     
 conjunto = set(lista)
 for valor in conjunto:
-    if lista.count(valor) == 1: print('O número ' + str(valor) + ' apareceu ' + str(lista.count(valor))+ ' vez.')
+    if lista.count(valor) == 1: print('O número ' + str(valor) + ' apareceu uma vez.')
     else: print('O número ' + str(valor) + ' apareceu ' + str(lista.count(valor))+ ' vezes.')
-
 print('Fim...')
